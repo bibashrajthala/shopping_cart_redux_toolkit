dbabc0bd5f66ac648c0a6d601dead352366b02ff
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-11-07 07:34:48 +0545

    implement cart increase, decrease, remove and clear with modal using redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
+import { useEffect } from "react";
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+import { useDispatch, useSelector } from "react-redux";
+
+import { calculateTotals } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
 }
 export default App;
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,15 +1,56 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
 
 const initialState = {
-  cartItems: [],
+  cartItems: cartItems,
   total: 0,
-  amount: 0,
+  amount: 4,
   isloading: false,
 };
 
 export const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = []; // directly mutate
+      // or return new state ,but whatever we return becomes new state
+      //eg if we return [], our state is now empty array, if we return {cartItems:[]} it is this object without total,amount,isloading , if we return {...state,cartItems:[]} ,then is same as previous but updates cartItems
+      //   return { ...state, cartItems: [] };
+    },
+    removeItem: (state, action) => {
+      //   console.log(action);
+      //   console.log(action.payload);
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, { payload }) => {
+      //   console.log(action.payload);
+      // console.log(payload)
+
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
+  },
 });
 
-console.log(cartSlice);
+// console.log(cartSlice);
+
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
